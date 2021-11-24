0d18783649fd4d0df3dc5c3b910e918c426a0bc2
bossie911 <62554007+bossie911@users.noreply.github.com>
2021-11-24 11:22:24 +0100

    zombie waypoint movement

---
+++ HammerTime/Assets/AIFollowWaypoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIFollowWaypoint : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    GameObject player;
+    GameObject currentWayPoint;
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentWayPoint = player.GetComponent<TempPlayer>().nodes[0];
+        agent.destination = currentWayPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == currentWayPoint)
+        {
+            index++;
+            currentWayPoint = player.GetComponent<TempPlayer>().nodes[index];
+            agent.destination = currentWayPoint.transform.position;
+        }
+    }
+}
---
+++ HammerTime/Assets/TempPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayer : MonoBehaviour
+{
+    public Rigidbody rb;
+    float speed = 8;
+
+    public GameObject node;
+
+    public List<GameObject> nodes = new List<GameObject>();
+
+    private void Awake()
+    {
+        GameObject p = Instantiate(node, transform.position, Quaternion.identity);
+        nodes.Add(p);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnWayPoints", 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var locVel = transform.InverseTransformDirection(rb.velocity);
+        locVel.z = speed;
+        rb.velocity = transform.TransformDirection(locVel);
+    }
+
+    void SpawnWayPoints()
+    {
+        GameObject p = Instantiate(node, transform.position, Quaternion.identity);
+        nodes.Add(p);
+    }
+}
