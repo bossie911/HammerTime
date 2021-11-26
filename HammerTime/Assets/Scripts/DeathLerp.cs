bf2466ee7bbeef0c2ab57edb083fe2631478c69e
Omega_The_III <jeroen.officier@acda.nl>
2021-11-26 15:21:05 +0100

    temp

---
+++ HammerTime/Assets/Scripts/DeathLerp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathLerp : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private int zoomInDuration;
+
+    private Vector3 offset;
+
+    public bool tempbool;
+    private void Update()
+    {
+        if (tempbool)
+        {
+            StartCoroutine(OnDeathLerpPlayer());
+            tempbool = false;
+        }
+    }
+    private IEnumerator OnDeathLerpPlayer()
+    {
+        float elapsed = 0;
+        while (elapsed < zoomInDuration)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, Player.transform.position + offset, elapsed / zoomInDuration);
+            yield return null;
+        }
+    }
+}
